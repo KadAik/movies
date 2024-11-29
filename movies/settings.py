33e0ebf7512e72c3069d89e11e0e0705e9ee92f7
Kad <learning.aik@gmail.com>
2024-11-29 15:48:33 +0100

    Finish periodic task scheduling with celery beat

--- movies/settings.py
+++ movies/settings.py
@@ -40,6 +40,7 @@ class Dev(Configuration):
     # Application definition
 
     INSTALLED_APPS = [
+        'django_celery_beat',
         'django_celery_results',
         'gh.apps.GhConfig',
         'moviesapp.apps.MoviesappConfig',
